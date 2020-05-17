345b04f605f5adef229b3c6ca810dc756c954341
rifqi yusrinald <rifqiyusrinaldi@gmail.com>
2020-05-17 18:57:55 +0700

    Project Test

---
+++ app/src/main/java/com/rifqi/shopingapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rifqi.shopingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
